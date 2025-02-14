42294a47656d51cc8354f983e6afb0fcc696fa08
fajrintanah <fajrintanah@gmail.com>
2025-02-14 16:02:30 +0700

    fifth try on mlp3 with narrowing other hyperparameters except hidden_units

--- Main_Code/4_Modelling_mlp_3.R
+++ Main_Code/4_Modelling_mlp_3.R
@@ -233,3 +233,58 @@ registerDoSEQ()
 
 # Show best combinations
 show_best(grid_results_N2_3, n = 10, metric = "rmse")
+
+autoplot(grid_results_N2_3)+coord_cartesian(ylim = c(3000, 3100))
+
+# epoch ranging from 1800-2000 seems to yield lower rmse
+# good hidden layers = 355
+# penalty at -1.6 to -1.55 seems to yield lower rmse
+# learning rate at -0.2 to -0.1 seems to yield lower rmse
+
+
+
+######### ------- fifth try ------- #########
+
+# 8. Efficient Parallel Setup -------------------------------------------------
+cl <- makePSOCKcluster(max(1, parallel::detectCores() - 2))  # Safer core allocation
+registerDoParallel(cl)
+
+# 9. Randomized Grid Search ---------------------------------------------------
+set.seed(123)
+folds_N2_4 <- vfold_cv(train_data_N, v = 5)
+
+set.seed(456)
+param_grid_N2_4 <- grid_random(
+  epochs(range = c(1800, 2000)),
+  hidden_units(range = c(300, 500)),
+  penalty(range = c(-1.6, -1.55)),
+  learn_rate(range = c(-0.2,-0.1)),
+  size = 30  # maybe lower the random combinations
+)
+
+# 10. Memory-Optimized Tuning --------------------------------------------------
+grid_results_N2_4 <- tune_grid(
+  mlp_wflow_tune_N2,
+  resamples = folds_N2_4,
+  grid = param_grid_N2_4,
+  metrics = metric_set(yardstick::rmse, yardstick::mae),
+  control = control_grid(
+    verbose = TRUE,
+    parallel_over = "everything",
+    allow_par = TRUE,
+    extract = NULL,        # No model extracts
+    save_pred = FALSE,     # No predictions storage
+    save_workflow = FALSE, # No workflow copies
+    pkgs = c("brulee")     # Minimal worker packages
+  )
+)
+
+# 11. Cleanup & Results --------------------------------------------------------
+stopCluster(cl)
+registerDoSEQ()
+
+# Show best combinations
+show_best(grid_results_N2_4, n = 10, metric = "rmse")
+
+autoplot(grid_results_N2_4)+coord_cartesian(ylim = c(3000, 3100))
+
