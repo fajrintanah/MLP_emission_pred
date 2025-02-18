f71259cf1a4fb6f2e369f87b84ced59955559943
fajrintanah <fajrintanah@gmail.com>
2025-02-18 22:35:28 +0700

    early prep script for K prep

---
+++ Main_Code/modelling_K/1_Preparation_K.R
@@ -0,0 +1,63 @@
+### ---------------- SKRIP MAKRO BARU FIX ------------------------
+
+
+library(tidyverse) # plotting and manipulation
+library(grid) # combining plots
+library(gridExtra) # combining plots
+library(ggpubr) # combining plots
+library(patchwork) # combining plots
+library(ggfortify) # nice extension for ggplot
+library(mgcv) #fitting gam models
+library(GGally) # displaying pairs panel
+library(caret)
+library(caTools) # split dataset
+library(readxl)
+library(randomForest)
+library(e1071)
+library(gbm)          # basic implementation
+library(xgboost)      # a faster implementation of gbm
+library(caret)        # an aggregator package for performing many machine learning models
+library(pdp)          # model visualization
+library(lime)         # model visualization
+library(neuralnet)
+library(rpart)     #rpart for computing decision tree models
+library(rsample)     # data splitting 
+library(dplyr)       # data wrangling
+library(rpart.plot)  # plotting regression trees
+library(ipred)       # bagging
+library(broom)
+library(ranger) 	#efficient RF
+library(NeuralNetTools)
+library(tidymodels)
+library(earth) 		#MARS model
+library(iml)		#most robust and efficient relative importance 
+library(xgboost)	#extreeme gradient boosting
+library(ModelMetrics) #get model metrics
+library(Metrics) 	#get ML model metrics
+library(Cubist) #Cubist modell
+library(iBreakDown)
+library(DALEX)
+library(viridis)
+library(ICEbox)
+library(hrbrthemes)
+library(tidyverse)
+library(hrbrthemes)
+library(viridis)
+library(vip)
+library(fastDummies)
+library(brulee )
+
+# -------------------------- Modelling for N -------------------------------------------------
+
+
+library(readxl)
+R_macro <- read_excel("E:/Fajrin/Publikasi/Pak Heru B Pulunggono/0 Road to Prof/18 Predicting Macronutrient in peat using ML/Data/R_macro.xlsx", 
+    col_types = c("text", "text", "skip", 
+        "text", "numeric", "numeric", "numeric", 
+        "numeric", "numeric", "numeric", 
+        "numeric", "numeric", "numeric", 
+        "numeric", "numeric", "numeric"))
+str(R_macro)
+
+
+
